ab922cb2fc1f7df66b88db734f8fc26622cbfbcd
MalcolmGC <SE.Consulting.Services@gmail.com>
2016-06-19 03:13:08 -0700

    16a. display info for one project. Local menus do not operate correctly. Old heroku menus do.

--- server.js
+++ server.js
@@ -1,53 +1,10 @@
 //Step 2. Create a server.js file and add the base route:
 var express = require('express'),
   app = express(),
+  exphbs = require('express-handlebars'),
+  githubService = require('./services/githubService.js'),
+  projectInfoService = require('./services/projectInfoService.js'), // New.
   port = process.env.PORT || 3000;
-// Server:
-app.listen(port, function() {
-  console.log("Server running on port", port);
-});
-// Routes:
-app.get('/', function (req, res) {
-  var favoriteLetters = ['a', 'b', 'c']; // TODO: Make my favorites here.
-  var favoriteLinks = [
-    { text: 'Apple', url: 'http://apple.com' },
-    { text: 'Facebook', url: 'http://facebook.com' }
-  ];
-  res.render('home', {
-    title: "Malcolm's Site",
-    favorites: favoriteLetters,
-    links: favoriteLinks
-  });
-});
-// /projects
-// app.get('/projects', function (req, res) {
-//   githubService.githubInfo()
-//   .then(function (results) {
-//     res.render('projects',
-//       { title: "Malcolm's Projects",
-//         bio: results.bio,
-//         repos: results.repos
-//       }
-//     );
-//   })
-//   .catch(function (err) {
-//     console.log('err: ', err);
-//   });
-// });
-// /projects Step 15:
-app.get('/projects/:id?', function (req, res) {
-  var currentProjectName = req.params.id; // id from the url.
-  console.log('currentProjectName:', currentProjectName);
-  res.render('projects', // view = file path of the view to render.
-    { // [, locals] [, callback])
-      title: "Malcolm's Projects: " + currentProjectName,
-      project: { name: currentProjectName } // pass to template.
-    }
-  );
-});
-var exphbs = require('express-handlebars');
-var githubService = require('./services/githubService.js');
-var port = process.env.PORT || 3000;
 // =======================
 // middleware & config
 // =======================
@@ -59,8 +16,94 @@ app.engine('hbs', exphbs({
   helpers: { // Added in Step 9.
     json: function (context) {
       return JSON.stringify(context);
+    //},
+    //formatDate: function (date, format) {
+    //  return moment(date).format(format);
     }
   }
 }));
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
+
+// Server:
+app.listen(port, function() {
+  console.log("Server running on port", port);
+});
+// Routes:
+app.get('/', function (req, res) {
+  var favoriteLetters = ['a', 'b', 'c']; // TODO: Make my favorites here.
+  var favoriteLinks = [
+    { text: 'Apple', url: 'http://apple.com' },
+    { text: 'Facebook', url: 'http://facebook.com' }
+  ];
+  console.log('Render home first link:', favoriteLinks[0].url);
+  res.render('home', {
+    title: "Malcolm's Site",
+    favorites: favoriteLetters,
+    links: favoriteLinks
+  });
+});
+//projects [Broken, but probably not here.]
+app.get('/projects', function (req, res) {
+  console.log("server: In '/projects' ..."); // OK.
+  githubService.githubInfo()
+  .then(function (results) {
+    var repos = results.repos;
+    console.log('server: repos:'); // Not shwowing.
+    console.log(repos);
+    repos.forEach(function (repo, index) {
+      repos[index].hasPost = projectInfoService.fileExists(repo.name);
+    });
+    console.log('server: results:'. results);
+    console.log('server: results.bio:'. results.bio);
+    res.render('projects',
+      { title: "Malcolm's Projects", // view = file path of the view to render.
+                              //   { // [, locals] [, callback])
+        bio: results.bio,
+        repos: results.repos
+      }
+    );
+  })
+  .catch(function (err) {
+    console.log('server: /projects err ... Getting results:');
+    console.log('server: console.log(results); not working');
+    console.log(results); // Not showing.
+    console.log('server: Getting error 401'); // Not logging.
+    // console.log('err: ', err);
+  });
+});
+// /project Step 15:
+app.get('/projects/:id', function (req, res) {
+  var currentProjectName = req.params.id; // id from the url.
+  console.log('In get for /projects/:id - currentProjectName:', currentProjectName);
+  var currentProject = {}; // New Step 16 ...
+  projectInfoService.readFile(currentProjectName, function (err, results) {
+    if (err) {
+      currentProject = {
+        post: currentProjectName + ' is invalid project name.'
+      };
+    } else {
+      //console.log('readFile results: ', results); // Buffer?
+      currentProject = {
+        name: currentProjectName,
+        post: results,
+        url: 'https://github.com/MalcolmGC/' + currentProjectName
+      };
+    }
+    //console.log('projectInfoService.readFile results:' , results ); // Buffer?
+    console.log('currentProject.name:' ,currentProject.name );
+    // console.log('currentProject.post:' ,currentProject.post ); // Buffer?
+    console.log('currentProject.url:' ,currentProject.url );
+    res.render('project', { // view = file path of the view to render.
+                    // [, locals]
+      title: "Malcolm's Project: " + currentProjectName, 
+      project: currentProject // pass to template.
+    });  // [, callback]
+  }); // End New.
+  // res.render('project',
+  //   {
+  //     title: 'My Projects: ' + currentProjectName,
+  //     project: { name: currentProjectName }
+  //   }
+  // );
+});
--- services/githubService.js
+++ services/githubService.js
@@ -1,4 +1,4 @@
-// /services/githubService.js
+// /services/githubService.js *** Looks OK.
 var axios = require('axios');
 if (process.env.NODE_ENV === 'development') {
   require('dotenv').config();
@@ -10,36 +10,37 @@ var githubService = function () {
       Authorization: 'token ' + process.env.GITHUB_TOKEN
     }
   };
+  console.log('githubService: ...');
   function getBio() { // returns a promise:
     // return axios.get('https://api.github.com/users/wykhuh', options);
-    return axios.get('https://api.github.com/users/MalcolmGC', options);
+    console.log('githubService: -- getBio ...'); // Promise
+    var axBio = axios.get('https://api.github.com/users/MalcolmGC', options);
+    // console.log('githubService: axBio', axBio);
+    return axBio;
   }
   function getRepos() { // Added
-    return axios.get('https://api.github.com/users/MalcolmGC/repos', options);
+    console.log('githubService: -- getRepos ...');
+    var axRepos = axios.get('https://api.github.com/users/MalcolmGC/repos', options);
+    // console.log('githubService: axRepos', axRepos); // Promise
+    return axRepos; 
   }
-
   function githubInfo() {
+    console.log('githubService:  githubInfo -- waiting for axios.all ...');
     return axios.all([getRepos(), getBio()]) // array of promises.
       .then(function (results) { // array of data.
+        console.log('githubService: githubInfo -- Promises done.*******');
         var repos = results[0].data;
         var bio = results[1].data;
+        console.log('githubService: -- results:', results); // Not getting here.
         return { repos: repos, bio: bio }; // object with repos and bio data.
       }
     );
   }
-
   return {
     getBio: getBio,
     getRepos: getRepos,
     githubInfo: githubInfo // Added
   };
-
-
-
-
-
-
-
 };
 
 module.exports = githubService();
